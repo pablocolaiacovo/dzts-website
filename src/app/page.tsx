0ae34671f7a5ac0fb7d442b74b09bed2947e7ee4
Eugenia Saravia <63812926+Euge-Saravia@users.noreply.github.com>
2025-05-09 17:00:48 +0200

    Create PropertyCard component with Bootstrap utilities and responsive design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import SearchProperties from "@/components/SearchProperties";
-
+import PropertyCard from "@/components/PropertyCard";
 export default function Home() {
   return (
+    <>
    <SearchProperties />
+   <PropertyCard />
+    </>
   );
 }
